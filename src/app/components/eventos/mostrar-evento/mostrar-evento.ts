01b8b2243930443b1c9a59aa9ce6cd0617d2b65d
allan <eraxoallan048@gmail.com>
2026-05-21 08:17:05 -0500

    Agregado el componente para mostrar eventos

--- src/app/components/eventos/mostrar-evento/mostrar-evento.ts
+++ src/app/components/eventos/mostrar-evento/mostrar-evento.ts
@@ -1,20 +1,35 @@
-import { Component, inject, signal, } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Evento } from '../../../models/evento.model';
 import { ServEventsJsonService } from '../../../services/serv-events-json.service';
 import { ActivatedRoute } from '@angular/router';
+import { ComentariosComponent } from "../../comentarios/comentarios";
 
 @Component({
   selector: 'app-mostrar-evento',
-  imports: [],
+  imports: [ComentariosComponent],
   templateUrl: './mostrar-evento.html',
   styleUrl: './mostrar-evento.css',
 })
 export class MostrarEvento {
-
-  evento = signal<Evento | null>(null); //Empieza vacío 
+  //Signals vacíos
+  evento = signal<Evento | null>(null);
+  tipoEvento = signal<string>('Cargando tipo...');
 
   private servicoEventos = inject(ServEventsJsonService);
 
-  constructor(private route:ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    //Obtener el ID de entrada
+    const id = Number(this.route.snapshot.paramMap.get('id') ?? '');
+
+    //llama al servicio con el ID que recibio
+    this.servicoEventos.getEventoPorId(id).subscribe((data) => {
+      this.evento.set(data);
+
+      this.servicoEventos.getTipoEventoPorId(data.tipoEventoId).subscribe((tipo) => {
+        this.tipoEvento.set(tipo.nombre);
+      });
+    });
+  }
 }
--- src/app/services/serv-events-json.service.ts
+++ src/app/services/serv-events-json.service.ts
@@ -19,11 +19,15 @@ export class ServEventsJsonService {
     return this.http.get<Evento[]>(this.eventosUrl);
   }
 
+  getEventoPorId(id:number): Observable<Evento> {
+    return this.http.get<Evento>(`${this.eventosUrl}/${id}`);
+  }
+
   getTipoEventos(): Observable<TipoEvento[]> {
     return this.http.get<TipoEvento[]>(this.tiposEventosUrl);
   }
 
-  getEventoPorId(id:string): Observable<Evento> {
-    return this.http.get<Evento>(`${this.eventosUrl}/${id}`);
+  getTipoEventoPorId(id:number): Observable<TipoEvento> {
+    return this.http.get<TipoEvento>(`${this.tiposEventosUrl}/${id}`);
   }
 }
