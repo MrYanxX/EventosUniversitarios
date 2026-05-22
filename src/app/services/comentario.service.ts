8d9d30e04716f1cae040c5fe16a165b97028bee0
Josue <josueidrovo123@gmail.com>
2026-05-22 11:39:24 -0500

    Mainx3 a

--- src/app/services/comentario.service.ts
+++ src/app/services/comentario.service.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Comentario } from '../models/comentario.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Comentario {}
+export class ComentarioService {
+  private apiUrl = 'http://localhost:3000/comentarios';
+  private http = inject(HttpClient); 
+
+  obtenerComentarios(): Observable<Comentario[]> {
+    return this.http.get<Comentario[]>(this.apiUrl);
+  }
+
+  crearComentario(comentario: Omit<Comentario, 'id'>): Observable<Comentario> {
+    return this.http.post<Comentario>(this.apiUrl, comentario);
+  }
+
+
+  actualizarComentario(id: string, comentario: Omit<Comentario, 'id'>): Observable<Comentario> {
+    return this.http.put<Comentario>(`${this.apiUrl}/${id}`, comentario);
+  }
+
+  eliminarComentario(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
