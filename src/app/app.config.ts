e9bce4447fd6e69fce7c2b98701be860b19b3474
MIGUEL ANGEL M <150745070+DarkinMiguel@users.noreply.github.com>
2026-07-18 00:01:41 -0500

    Cambios interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http'; 
 import { provideRouter } from '@angular/router';
-  
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-     provideHttpClient()
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,37 +1,37 @@
-import { Routes } from '@angular/router';
+    import { Routes } from '@angular/router';
 
-import { HomeContenedor } from './components/home/home-contenedor/home-contenedor';
-import { ComentariosComponent } from './components/comentarios/comentarios';
-import { MostrarEvento } from './components/eventos/mostrar-evento/mostrar-evento';
-import { FormEventos } from './components/eventos/form-eventos/form-eventos';
-import { TablaEventos } from './components/eventos/tabla-eventos/tabla-eventos';
+    import { HomeContenedor } from './components/home/home-contenedor/home-contenedor';
+    import { ComentariosComponent } from './components/comentarios/comentarios';
+    import { MostrarEvento } from './components/eventos/mostrar-evento/mostrar-evento';
+    import { FormEventos } from './components/eventos/form-eventos/form-eventos';
+    import { TablaEventos } from './components/eventos/tabla-eventos/tabla-eventos';
 
-import { CuentaComponent } from './components/globales/cuenta/cuenta';
+    import { CuentaComponent } from './components/globales/cuenta/cuenta';
 
-import { CrudPonentes } from './components/ponentes/crud-ponentes/crud-ponentes';
-import { CrudRecursos } from './components/recursos/crud-recursos/crud-recursos';
+    import { CrudPonentes } from './components/ponentes/crud-ponentes/crud-ponentes';
+    import { CrudRecursos } from './components/recursos/crud-recursos/crud-recursos';
 
-import { CrudOrganizadores } from './components/organizadores/crud-organizadores/crud-organizadores';
-import { DashboardOrganizador } from './components/organizadores/dashboard-organizador/dashboard-organizador';
-import { ListaInscripciones } from './components/inscripciones/lista-inscripciones/lista-inscripciones';
-import { FormInscripcion } from './components/inscripciones/form-inscripcion/form-inscripcion';
+    import { CrudOrganizadores } from './components/organizadores/crud-organizadores/crud-organizadores';
+    import { DashboardOrganizador } from './components/organizadores/dashboard-organizador/dashboard-organizador';
+    import { ListaInscripciones } from './components/inscripciones/lista-inscripciones/lista-inscripciones';
+    import { FormInscripcion } from './components/inscripciones/form-inscripcion/form-inscripcion';
 
-export const routes: Routes = [
+    export const routes: Routes = [
 
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeContenedor },
-    { path: 'comentarios', component: ComentariosComponent },
-    { path: 'evento/:id', component: MostrarEvento },
-    { path: 'crear-evento', component: FormEventos },
-    { path: 'editar-evento/:id', component: FormEventos },
-    { path: 'eventos', component: TablaEventos },
-    { path: 'ponentes', component: CrudPonentes },
-    { path: 'recursos', component: CrudRecursos },
-    { path: 'cuenta', component: CuentaComponent },
-    { path: 'organizadores', component: CrudOrganizadores },
-    { path: 'dashboard-organizador', component: DashboardOrganizador },
-    { path: 'inscripciones', component: ListaInscripciones },
-    { path: 'nueva-inscripcion/:id', component: FormInscripcion },
-    { path: '**', redirectTo: 'home' }
+        { path: '', redirectTo: 'home', pathMatch: 'full' },
+        { path: 'home', component: HomeContenedor },
+        { path: 'comentarios', component: ComentariosComponent },
+        { path: 'evento/:id', component: MostrarEvento },
+        { path: 'crear-evento', component: FormEventos },
+        { path: 'editar-evento/:id', component: FormEventos },
+        { path: 'eventos', component: TablaEventos },
+        { path: 'ponentes', component: CrudPonentes },
+        { path: 'recursos', component: CrudRecursos },
+        { path: 'cuenta', component: CuentaComponent },
+        { path: 'organizadores', component: CrudOrganizadores },
+        { path: 'dashboard-organizador', component: DashboardOrganizador },
+        { path: 'inscripciones', component: ListaInscripciones },
+        { path: 'nueva-inscripcion/:id', component: FormInscripcion },
+        { path: '**', redirectTo: 'home' }
 
-]; 
+    ]; 
