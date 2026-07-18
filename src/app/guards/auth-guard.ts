ef5467b2da254c3b0ed61997d16db4e76b6d1174
Angel Mora <moraa9565@gmail.com>
2026-07-18 11:09:27 -0500

    CAMBIOS DE AUTH GUARD 1

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,5 +1,18 @@
 import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = () => {
+
+  const router = inject(Router);
+
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    return true;
+  }
+
+  router.navigate(['/cuenta']); // Cambia esta ruta si tu login está en otra
+
+  return false;
 };
